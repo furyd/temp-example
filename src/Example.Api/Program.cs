fa940d991f0363ab23bd7e66ee8c213723876f73
davefury <df@bridgevms.com>
2023-08-17 17:09:00 +0100

    feat: removed program from code coverage and made it hookable into web application factory

--- src/Example.Api/Program.cs
+++ src/Example.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 using Example.Api.Services.Implementation;
 using Example.Domain.Queries.Registration;
@@ -35,3 +36,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+[ExcludeFromCodeCoverage]
+public partial class Program { }
